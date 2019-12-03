8f438b4c25d3dce0a94de1200b298e65f3d04226
Llovet Nicholas <nllovet20199@columbushs.com>
2019-12-03 10:30:47 -0500

    start

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,22 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+         this.health = health;
+         this.strength = strength;
+        }
+         attack = function() {
+             return this.strength;
+    }
+        receiveDamage = function(damage) {
+            this.health -= damage;
+        }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, heatlh, strength);
+    super(health, strength);
+}
 
 // Saxon
 class Saxon {}
