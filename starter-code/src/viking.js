a6c6da2bc21ab5add4f777197a8a0b6a7c7cd8ff
Llovet Nicholas <nllovet20199@columbushs.com>
2019-12-03 15:25:53 -0500

    finish

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -14,12 +14,49 @@ class Soldier {
 
 // Viking
 class Viking extends Soldier {
-    constructor(name, heatlh, strength);
-    super(health, strength);
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+    attack = function() {
+        return this.strength;
+    }
+    receiveDamage = function(damage) {
+        this.health -= damage;
+        if (this.health <= 0) {
+            return (`${this.name} has died in act of combat`);
+        }
+        else{
+            return (`${this.name} has received ${damage} points of damage`);
+        }
+    }
+
+    battleCry = function() {
+        return "Odin Owns You All!";
+    }
+    
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    constructor(health, strength) {
+        super(health, strength);
+        this.health = health;
+        this.strength = strength;
+    }
 
-// War
+    receiveDamage = function(damage) {
+        this.health -= damage;
+        if(this.health <= 0) {
+            return (`A Saxon has died in combat`);
+        }
+        else{
+            return (`A Saxon has received ${damage} points of damage`);
+        }
+    }
+}
+
+// War BONUS
 class War {}
